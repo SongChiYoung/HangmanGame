8ea9270b3e6866ec955d23fa40c364032c4f7f91
Chiyoung.Song <chiyoung.song@motov.co.kr>
2023-10-02 08:15:18 +0900

    FEAT: Mission aware
    6.
    일치하는 알파벳이 있을 경우 기존 UI에서 사라지고 해당 위치에 표시됩니다.
    7.
    틀릴 경우 기존 위치에서 알파벳이 빨간색으로 바뀌고, 오류 횟수를 증가시킵니다.
    8.
    오류 날 때마다 ‘교수대-밧줄-머리-팔-손-몸통-다리-발’ 순서로 그려서 그림이 완성되면 ‘실패’ 를 띄웁니다. (오류 횟수 8번 이상은 실패)

--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ function App() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map((letter, index) => (
       <button class="button-alphabet" key={index} onClick={() => checkAlphabet(letter)}
         disabled={disabledAlphabets.includes(letter)}
+        style={{visibility: disabledAlphabets.includes(letter) && selectedWord.includes(letter) ? 'hidden' : 'visible'}}
       >
         {letter}
       </button>
@@ -62,33 +63,67 @@ function App() {
   const renderHangingMan = () => {
     return (
       <svg height="250" width="200">
-          {/* Base */}
-          <line x1="50" y1="200" x2="150" y2="200" style={{stroke:"#000", strokeWidth:2}} />
-          {/* Vertical Pole */}
-          <line x1="100" y1="200" x2="100" y2="50" style={{stroke:"#000", strokeWidth:2}} />
-          {/* Horizontal Pole */}
-          <line x1="100" y1="50" x2="150" y2="50" style={{stroke:"#000", strokeWidth:2}} />
-          {/* Rope */}
-          <line x1="140" y1="50" x2="140" y2="70" style={{stroke:"#000", strokeWidth:2}} />
-          {/* Head */}
-          {errorCount > 0 && <circle cx="140" cy="80" r="10" style={{stroke:"#000", strokeWidth:2, fill:"#fff"}} />}
-          {/* Body */}
-          {errorCount > 1 && <line x1="140" y1="90" x2="140" y2="120" style={{stroke:"#000", strokeWidth:2}} />}
-          {/* Left Arm */}
-          {errorCount > 2 && <line x1="140" y1="90" x2="120" y2="110" style={{stroke:"#000", strokeWidth:2}} />}
-          {/* Right Arm */}
-          {errorCount > 3 && <line x1="140" y1="90" x2="160" y2="110" style={{stroke:"#000", strokeWidth:2}} />}
-          {/* Left Eye */}
-          {errorCount > 4 && <circle cx="137" cy="78" r="1" style={{stroke:"#000", strokeWidth:1, fill:"#000"}} />}
-          {/* Right Eye */}
-          {errorCount > 5 && <circle cx="143" cy="78" r="1" style={{stroke:"#000", strokeWidth:1, fill:"#000"}} />}
-          {/* Left Leg */}
-          {errorCount > 6 && <line x1="140" y1="120" x2="120" y2="140" style={{stroke:"#000", strokeWidth:2}} />}
-          {/* Right Leg */}
-          {errorCount > 7 && <line x1="140" y1="120" x2="160" y2="140" style={{stroke:"#000", strokeWidth:2}} />}
-        </svg>
+        {/* Base */}
+        <line x1="50" y1="200" x2="150" y2="200" style={{stroke:"#000", strokeWidth:2}} />
+        {/* Vertical Pole */}
+        {errorCount > 0 && <line x1="100" y1="200" x2="100" y2="50" style={{stroke:"#000", strokeWidth:2}} />}
+        {/* Horizontal Pole */}
+        {errorCount > 0 && <line x1="100" y1="50" x2="150" y2="50" style={{stroke:"#000", strokeWidth:2}} />}
+        {/* Rope */}
+        {errorCount > 1 && <line x1="140" y1="50" x2="140" y2="70" style={{stroke:"#000", strokeWidth:2}} />}
+        {/* Head */}
+        {errorCount > 2 && <circle cx="140" cy="80" r="10" style={{stroke:"#000", strokeWidth:2, fill:"#fff"}} />}
+        {/* Arms */}
+        {errorCount > 3 && <line x1="140" y1="90" x2="160" y2="110" style={{stroke:"#000", strokeWidth:2}} />}
+        {errorCount > 3 && <line x1="140" y1="90" x2="120" y2="110" style={{stroke:"#000", strokeWidth:2}} />}
+        {/* Hands */}
+        {errorCount > 4 && <circle cx="162" cy="112" r="2" style={{stroke:"#000", strokeWidth:2, fill:"#fff"}} />}
+        {errorCount > 4 && <circle cx="118" cy="112" r="2" style={{stroke:"#000", strokeWidth:2, fill:"#fff"}} />}   
+        {/* Body */}
+        {errorCount > 5 && <line x1="140" y1="90" x2="140" y2="120" style={{stroke:"#000", strokeWidth:2}} />}
+        {/* Legs */}
+        {errorCount > 6 && <line x1="140" y1="120" x2="160" y2="140" style={{stroke:"#000", strokeWidth:2}} />}
+        {errorCount > 6 && <line x1="140" y1="120" x2="120" y2="140" style={{stroke:"#000", strokeWidth:2}} />}
+        {/* Feet */}
+        {errorCount > 7 && (
+          <>
+            {/* Right Foot */}
+            <ellipse cx="162" cy="142" rx="4" ry="2" style={{stroke:"#000", strokeWidth:2, fill:"#fff"}} />
+            {/* Left Foot */}
+            <ellipse cx="118" cy="142" rx="4" ry="2" style={{stroke:"#000", strokeWidth:2, fill:"#fff"}} />
+          </>
+        )}
+        {/* Eyes */}
+        {errorCount > 2 && errorCount <= 7 && (
+          <>
+            <circle cx="137" cy="78" r="1" style={{fill:"#000"}} />
+            <circle cx="143" cy="78" r="1" style={{fill:"#000"}} />
+          </>
+        )}
+        {/* Angry Eyebrows when 5 errors */}
+        {errorCount > 4 && errorCount <= 7 && (
+          <>
+            <line x1="136" y1="76" x2="138" y2="74" style={{stroke:"#000", strokeWidth:1}} />
+            <line x1="144" y1="76" x2="142" y2="74" style={{stroke:"#000", strokeWidth:1}} />
+          </>
+        )}
+        {/* Mouth */}
+        {errorCount > 2 && errorCount <= 7 && (
+          <line x1="137" y1="83" x2="143" y2="83" style={{stroke:"#000", strokeWidth:1}} />
+        )}
+        {errorCount > 7 && (
+          <>
+            {/* Eyes turned into X */}
+            <line x1="136" y1="77" x2="138" y2="79" style={{stroke:"#000", strokeWidth:1}} />
+            <line x1="136" y1="79" x2="138" y2="77" style={{stroke:"#000", strokeWidth:1}} />
+            <line x1="142" y1="77" x2="144" y2="79" style={{stroke:"#000", strokeWidth:1}} />
+            <line x1="142" y1="79" x2="144" y2="77" style={{stroke:"#000", strokeWidth:1}} />
+          </>
+        )}
+      </svg>
     );
   };
+  
 
   return (
     <div id="Game">
